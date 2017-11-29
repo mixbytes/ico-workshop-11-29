c0d801ffbff13df30281ac7019fa09bd44bc6d4c
Eenae <z-line@my.com>
2017-11-29 22:24:28 +0300

    networks

--- truffle.js
+++ truffle.js
@@ -4,6 +4,20 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+
+    rinkeby: {  // testnet
+      host: "localhost",
+      port: 8547,
+      network_id: 4
+    },
+
+    mainnet: {
+      host: "localhost",
+      port: 8549,
+      network_id: 1,
+      gasPrice: 7000000000
     }
+
   }
 };
