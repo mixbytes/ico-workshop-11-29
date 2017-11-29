6ade17cc19fe154949535e86a4317e3544cfa41a
Eenae <z-line@my.com>
2017-11-29 21:13:56 +0300

    processing investments

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,11 +1,13 @@
 pragma solidity ^0.4.15;
 
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 
 import './Token.sol';
 
 
 contract Crowdsale is Ownable {
+    using SafeMath for uint;
 
     enum State {INIT, RUNNING, PAUSED, FAILED, SUCCEEDED}
 
@@ -15,10 +17,41 @@ contract Crowdsale is Ownable {
         _;
     }
 
+    modifier timedStateChange() {
+        if (m_state == State.INIT && now >= c_start_time)
+            changeState(State.RUNNING);
+        _;
+    }
+
     // PUBLIC
 
     function Crowdsale(){
         m_token = new Token();
+    }
+
+    function() public payable {
+        buy();
+    }
+
+    function buy()
+        public
+        payable
+        timedStateChange
+        requiresState(State.RUNNING)
+    {
+        // msg.sender sends msg.value ether
+        if (now > c_end_time) {
+            finish();
+            msg.sender.transfer(msg.value);
+        }
+        else {
+            assert(now >= c_start_time && now <= c_end_time);
+
+            uint tokens = msg.value.mul(c_tokens_per_wei);
+
+            require(tokens <= m_token.balanceOf(this));
+            m_token.transfer(msg.sender, tokens);
+        }
     }
 
 
@@ -40,8 +73,20 @@ contract Crowdsale is Ownable {
         m_state = new_state;
     }
 
+    function finish() internal {
+        assert(false);
+    }
+
 
     // FIELDS
+
+    uint public constant c_start_time = 1512086400;
+    uint public constant c_end_time = c_start_time + 30 days;
+
+    uint public constant c_tokens_per_wei = 100;
+
+    uint public constant c_soft_cap = 1000 ether;
+
 
     Token public m_token;
     State m_state = State.INIT;
