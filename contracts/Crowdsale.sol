e1604c8ec1482908dfde0d106e9c386e653601f2
Eenae <z-line@my.com>
2017-11-29 20:44:00 +0300

    state machine

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -6,9 +6,43 @@ import './Token.sol';
 
 
 contract Crowdsale is Ownable {
+
+    enum State {INIT, RUNNING, PAUSED, FAILED, SUCCEEDED}
+
+
+    modifier requiresState(State state) {
+        require(m_state == state);
+        _;
+    }
+
+    // PUBLIC
+
     function Crowdsale(){
         m_token = new Token();
     }
 
+
+    // INTERNAL
+
+    function changeState(State new_state) internal {
+        assert(new_state != m_state);
+
+        if (m_state == State.INIT) {
+            assert(new_state == State.RUNNING);
+        }
+        else if (m_state == State.RUNNING) {
+            assert(new_state == State.PAUSED || new_state == State.FAILED || new_state == State.SUCCEEDED);
+        }
+        else {
+            // ...
+        }
+
+        m_state = new_state;
+    }
+
+
+    // FIELDS
+
     Token public m_token;
+    State m_state = State.INIT;
 }
